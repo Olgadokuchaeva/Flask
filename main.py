cf7b7b60e443b675f9b60269baf55509fbec5681
Ученик <olga1809dok@gmail.com>
2025-03-18 18:17:32 +0300

    start

--- main.py
+++ main.py
@@ -8,48 +8,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/mars_explorer.db")
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = "research enginner"
-    user.address = 'module_1'
-    user.email = "scott_chief@mars.org"
+    name = input()
+    #db_session.global_init("db/mars_explorer.db")
+    db_session.global_init(name)
     db_sess = db_session.create_session()
-    db_sess.add(user)
-    user1 = User()
-    user1.surname = "Dika"
-    user1.name = "Vika"
-    user1.age = 100
-    user1.position = 'player'
-    user1.speciality = "doing nothing"
-    user1.address = 'module_2'
-    user1.email = "vikadika@mars.org"
-    db_sess = db_session.create_session()
-    db_sess.add(user1)
-    user3 = User()
-    user3.surname = "Pupkin"
-    user3.name = "Vasy"
-    user3.age = 18
-    user3.position = 'kok'
-    user3.speciality = "cooking"
-    user3.address = 'module_3'
-    user3.email = "kokki@mars.org"
-    db_sess = db_session.create_session()
-    db_sess.add(user3)
-    user2 = User()
-    user2.surname = "Holl"
-    user2.name = "Tom"
-    user2.age = 20
-    user2.position = 'doctor'
-    user2.speciality = "helping"
-    user2.address = 'module_3'
-    user2.email = "doktom@mars.org"
-    db_sess = db_session.create_session()
-    db_sess.add(user2)
-    db_sess.commit()
+    user = db_sess.query(User).first()
+    for user in db_sess.query(User).filter(User.address.like("module_1")):
+        print(user)
+
     #app.run()
 
 
